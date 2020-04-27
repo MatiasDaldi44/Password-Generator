acfe51afff4db159ac504db9d44ab2a1de5d22e3
MD24 <matiasdaldi@gmail.com>
2020-04-27 19:53:58 -0400

    updated code removing the checkboxes

--- Java/script.js
+++ Java/script.js
@@ -1,23 +1,46 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 var yourPass = document.getElementById("password");
-var charNum = document.getElementById("charNum");
-var numBox = document.getElementById("num");
-var lowBox = document.getElementById("low");
-var specBox = document.getElementById("spec");
-
+var characters = "";
 var alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var alpha2 = "abcdefghijklmnopqrstuvwxyz";
 var numbers = "0123456789";
-var special = "!@#$%^&*";
+var special = "!@#$%^&*()-+=<>?~[]{}";
 
-generateBtn.addEventListener("click", function() { 
-  var characters = alpha;
-    (numBox.checked) ? characters += numbers : '';
-    (lowBox.checked) ? characters += alpha2 : '';
-    (specBox.checked) ? characters += special : '';
-    yourPass.value = generatePassword(charNum.value, characters);
-});
+var howMany = prompt("Welcome to your Random Password Generator. Please pick a number between 8-128 for your password.")
+
+startUp();
+
+generateBtn.addEventListener("click", function() {
+  yourPass.value = generatePassword(howMany, characters)
+})
+
+function startUp() {
+  if (howMany < 8 || howMany > 128) {
+    alert("Please reload and pick a number between 8-128")
+  } else {
+    var alphaConfirm = confirm("Would you like capital letter?")
+    var alpha2Confirm = confirm("Would you like lower case letters?")
+    var numbersConfirm = confirm("Would you like numbers?")
+    var specialConfirm = confirm("Would you like special characters?")
+  }
+
+  if (alphaConfirm === false && alpha2Confirm === false && numbersConfirm === false && specialConfirm === false) {
+    alert("You gotta click yes on something, man")
+  } 
+  if (alphaConfirm === true) {
+    characters += alpha
+  } 
+  if ( alpha2Confirm === true) {
+    characters += alpha2
+  } 
+  if (numbersConfirm === true) {
+    characters += numbers
+  } 
+  if (specialConfirm === true) {
+    characters += special
+  }
+}
 
 function generatePassword(l, characters) {
   var pwd = "";
@@ -25,8 +48,4 @@ function generatePassword(l, characters) {
     pwd += characters.charAt(Math.floor(Math.random() * characters.length));
   }
   return pwd;
-}
-
-function updateTextInput(val) {
-  document.getElementById('textInput').value=val; 
 }
